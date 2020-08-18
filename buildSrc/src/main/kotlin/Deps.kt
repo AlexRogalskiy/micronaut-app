5fecc2bac60a5b1d3bda313969221d24f8a7e8d0
Suresh G <sureshg.g@gmail.com>
2020-08-17 18:05:58 -0700

    chore: kotlin 1.4.0 upgrade

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -3,7 +3,7 @@ import org.gradle.plugin.use.*
 
 object Versions {
     // Deps
-    const val kotlin = "1.3.72"
+    const val kotlin = "1.4.0"
     const val coroutines = "1.3.7"
     const val kotlinSerialization = "0.20.0"
     const val kotlinxReflectLite = "1.0.0"
